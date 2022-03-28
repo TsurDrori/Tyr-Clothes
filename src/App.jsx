8abf6d8f9f598837f93b616795340a8289bdeb76
TsurDrori <tsurdrori@gmail.com>
2022-03-28 09:25:50 +0300

    finished basic structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>hats</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
