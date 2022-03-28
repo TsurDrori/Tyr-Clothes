faeae475a5680047ffd6b39efb84ec02c7e9e156
TsurDrori <tsurdrori@gmail.com>
2022-03-28 09:36:18 +0300

    map on array of categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-function App() {
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'hats',
+    },
+    {
+      id: 2,
+      title: 'snickers',
+    },
+    {
+      id: 3,
+      title: 'jackets',
+    },
+    {
+      id: 4,
+      title: 'woman',
+    },
+    {
+      id: 5,
+      title: 'man',
+    }
+  ]
+
   return (
     <div className="categories-container">
+{categories.map(({title}) => (
       <div className="category-container">
+        <div className="background-image">
         {/* <img src="" alt="" /> */}
+        </div>
         <div className="category-body-container">
-          <h2>hats</h2>
+          <h2>{title}</h2>
           <p>shop now</p>
         </div>
       </div>
+))}
     </div>
   );
 }
