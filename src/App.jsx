7da24bad0a986375887516bcaa4bff1ecc8f9d9d
TsurDrori <tsurdrori@gmail.com>
2022-03-29 10:28:10 +0300

    routing setup , firebase setup, google auth setup, header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import CategoryList from "./components/CategoryList";
-const App = () => {
+import { Routes, Route } from 'react-router-dom';
 
+import Home from './routes/Home.jsx';
+import Header from './routes/Header.jsx';
+import SignIn from './routes/SignIn.jsx';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
   return (
-    <div >
-      <CategoryList/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
